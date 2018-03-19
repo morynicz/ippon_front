5f196a344159bf0e7de53008bb4e1ca4569e5790
Michał Orynicz <morynicz@gmail.com>
2018-03-19 08:44:18 +0100

    Clear tokens on application start (for now)

--- src/app/authorization/token-storage.service.ts
+++ src/app/authorization/token-storage.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TokenStorageService {
 
-  constructor() { }
+  constructor() { this.clearTokens(); }
 
   getAccess() {
     return localStorage.getItem('access');
