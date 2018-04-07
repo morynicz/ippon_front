35f3cfbf673930ad09d0c7a68b415d38d8d33b49
Michał Orynicz <morynicz@gmail.com>
2018-04-07 14:25:07 +0200

    Add missing rest-api.ts

---
+++ src/app/rest-api.ts
@@ -0,0 +1,11 @@
+export const AUTHENTICATION_HOST: string = "http://localhost:8000";
+export const AUTHENTICATION_ENDPOINT: string = "/auth";
+export const TOKEN_ENDPOINT: string = "/token";
+
+export const IPPON_HOST: string = "http://localhost:8000/ippon/";
+export const AUTHORIZATION_ENDPOINT: string = "/authorization/";
+export const CLUBS_ENDPOINT: string = "clubs/";
+export const TOURNAMENTS_ENDPOINT: string = "tournaments/";
+export const ADMINS_ENDPOINT: string = "admins/";
+export const STAFF_ENDPOINT: string = "staff/";
+export const PLAYERS_ENDPOINT: string = "players/";
