2862115e18ab0700a948fba3980e4758f705209f
Michał Orynicz <morynicz@gmail.com>
2018-04-13 08:00:09 +0200

    Add tests for PlayerLine

--- src/app/player/player-line/player-line.component.spec.ts
+++ src/app/player/player-line/player-line.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 import { PlayerLineComponent } from './player-line.component';
 import { Player, Sex, Rank } from '../player';
@@ -8,6 +9,7 @@ describe('PlayerLineComponent', () => {
   let component: PlayerLineComponent;
   let fixture: ComponentFixture<PlayerLineComponent>;
   let expectedPlayer: Player;
+  let html;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PlayerLineComponent],
@@ -30,9 +32,24 @@ describe('PlayerLineComponent', () => {
     };
     component.player = expectedPlayer;
     fixture.detectChanges();
+    html = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("when created", () => {
+    it("should display player name", () => {
+      expect(html.textContent).toContain(expectedPlayer.name);
+    });
+    it("should display player surname", () => {
+      expect(html.textContent).toContain(expectedPlayer.surname);
+    });
+    it('should provide link to the player', () => {
+      const link = fixture.debugElement.query(By.css('a'));
+      expect(link.nativeElement.getAttribute('ng-reflect-router-link'))
+        .toBe('/player/' + expectedPlayer.id);
+    });
+  })
 });
