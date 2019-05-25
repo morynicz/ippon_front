3ccde5c127232c3e31d5629a4e9434cbfd4921d7
Michał Orynicz <morynicz@gmail.com>
2019-05-25 18:24:25 +0200

    Prepare scaffolding

---
+++ src/app/cup-phase/cup-phase-full/cup-phase-full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ippon-cup-phase-full',
+  templateUrl: './cup-phase-full.component.html',
+  styleUrls: ['./cup-phase-full.component.css']
+})
+export class CupPhaseFullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
