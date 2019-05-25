4b21c3d05ca90864c5fa4cd3bfbfdc861e6ca5ca
Michał Orynicz <morynicz@gmail.com>
2019-05-26 01:08:17 +0200

    Show names of all teams present in cup fights of cup phase in CupPhaseFull

--- src/app/cup-phase/cup-phase-full/cup-phase-full.component.ts
+++ src/app/cup-phase/cup-phase-full/cup-phase-full.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CupPhaseService } from '../cup-phase.service';
+import { CupPhase } from '../cup-phase';
+import { CupFightService } from '../../cup-fight/cup-fight.service';
+import { CupFight } from '../../cup-fight/cup-fight';
+import { cupFights, teams } from './test-scaffolding';
+import { Team } from '../../team/team';
+import { TeamService } from '../../team/team.service';
+import { TeamFight } from '../../team-fight/team-fight';
+import { TeamFightService } from '../../team-fight/team-fight.service';
 
 @Component({
   selector: 'ippon-cup-phase-full',
@@ -6,10 +16,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cup-phase-full.component.css']
 })
 export class CupPhaseFullComponent implements OnInit {
-
-  constructor() { }
+  cupPhase: CupPhase;
+  cupFights: CupFight[] = [];
+  teamFights: TeamFight[] = [];
+  teams: Team[] = [];
+  constructor(private route: ActivatedRoute,
+    private cupPhaseService: CupPhaseService,
+    private cupFightService: CupFightService,
+    private teamFightService: TeamFightService,
+    private teamService: TeamService) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.cupPhaseService.get(id).subscribe(resp => this.cupPhase = resp);
+    this.cupFightService.getList(id).subscribe(resp => this.cupFights = resp);
+    cupFights.forEach(cupFight => {
+      this.teamFightService.get(cupFight.team_fight).subscribe(resp => this.teamFights.push(resp));
+    });
+    this.teamFights.forEach(teamFight => {
+      this.teamService.get(teamFight.aka_team).subscribe(resp => this.teams.push(resp));
+      this.teamService.get(teamFight.shiro_team).subscribe(resp => this.teams.push(resp));
+    });
   }
 
 }
