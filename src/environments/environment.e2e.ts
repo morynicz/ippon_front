acda9659a8ef93dabb9370441d3b70fe25fe2f65
Michał Orynicz <morynicz@gmail.com>
2019-08-05 18:03:42 +0200

    Preparations for e2e testing

--- src/environments/environment.e2e.ts
+++ src/environments/environment.e2e.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    authHost: "https://ipp-back-e2e.herokuapp.com/",
-    ipponHost: "https://ipp-back-e2e.herokuapp.com/ippon/"
+    // authHost: "https://ipp-back-e2e.herokuapp.com/",
+    // ipponHost: "https://ipp-back-e2e.herokuapp.com/ippon/"
+    authHost: "http://localhost:8923/",
+    ipponHost: "http://localhost:8923/ippon/"
 };
